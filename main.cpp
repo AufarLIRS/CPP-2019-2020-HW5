fe5a104ab126787d57034d6faa5caaef0f786b91
Timophey Mironchuk <snakeinacartonbox@gmail.com>
2019-12-28 20:01:14 +0300

    .

--- main.cpp
+++ main.cpp
@@ -5,24 +5,23 @@ using namespace std;
 
 int main()
 {
-    vector<Counted*> countedList;
-    Counted* a = new Counted();
+  vector<Counted*> countedList;
+  Counted* a = new Counted();
 
-    Counted* b = new Counted();
-    Counted* c = new Counted();
-    Counted* d = new Counted();
-            delete d;
-    Counted* e = new Counted();
-        countedList.push_back(a);
-        countedList.push_back(b);
-        countedList.push_back(c);
-        countedList.push_back(e);
-       // countedList().push_back(c);
-       // countedList().push_back(d);
-       // countedList().push_back(e);
+  Counted* b = new Counted();
+  Counted* c = new Counted();
+  Counted* d = new Counted();
+  delete d;
+  Counted* e = new Counted();
+  countedList.push_back(a);
+  countedList.push_back(b);
+  countedList.push_back(c);
+  countedList.push_back(e);
+  // countedList().push_back(c);
+  // countedList().push_back(d);
+  // countedList().push_back(e);
 
+  cout << countedList.back()->getId();
 
-        cout<<countedList.back()->getId();
-
-    return 0;
+  return 0;
 }
